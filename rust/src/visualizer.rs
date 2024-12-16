c0be66bf378cfe11c136141b31e436cbbe5fab19
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-12-17 06:44:48 +1000

    changed the way I store the buffers in the visualizer

--- rust/src/visualizer.rs
+++ rust/src/visualizer.rs
@@ -1,62 +1,72 @@
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum Row {
-	Half,
-	Full,
+enum Buffer {
+	Index(usize),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum MiddleBuffer {
+	One(Buffer),
+	Two(Buffer, Buffer),
 }
 
 #[derive(Debug, PartialEq)]
 struct Grid {
-	top: usize,
-	middle: Option<Vec<Row>>,
-	bottom: usize,
+	top: Vec<Buffer>,
+	middle: Option<Vec<MiddleBuffer>>,
+	bottom: Option<Vec<Buffer>>,
 }
 
 impl Grid {
 	pub fn new(buffer: usize) -> Self {
 		match buffer {
-			0 => Self {
-				top: 0,
-				middle: None,
-				bottom: 0,
-			},
+			0 => panic!("Must have at least one buffer enabled"),
 			1 => Self {
-				top: 1,
+				top: vec![Buffer::Index(0)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			},
 			2 => Self {
-				top: 2,
+				top: vec![Buffer::Index(0), Buffer::Index(1)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			},
 			3 => Self {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			},
 			4 => Self {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 1,
+				bottom: Some(vec![Buffer::Index(3)]),
 			},
 			5 => Self {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 2,
+				bottom: Some(vec![Buffer::Index(3), Buffer::Index(4)]),
 			},
 			6 => Self {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 3,
+				bottom: Some(vec![Buffer::Index(3), Buffer::Index(4), Buffer::Index(5)]),
 			},
 			b => {
-				let n = ((b - 7) / 2);
-				let mut middle = vec![Row::Full; n];
-				middle.push(if b % 2 == 0 { Row::Full } else { Row::Half });
+				let n = (b - 7) / 2;
+				let mut middle = Vec::new();
+				for index in (0..n).step_by(2) {
+					middle.push(MiddleBuffer::Two(Buffer::Index(index), Buffer::Index(index + 1)));
+				}
+				let rest = b - n - 6;
+				middle.push(if b % 2 == 0 {
+					MiddleBuffer::Two(Buffer::Index(rest), Buffer::Index(rest + 1))
+				} else {
+					MiddleBuffer::One(Buffer::Index(rest))
+				});
+
 				Self {
-					top: 3,
+					top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 					middle: Some(middle),
-					bottom: 3,
+					bottom: Some(vec![Buffer::Index(b - 2), Buffer::Index(b - 1), Buffer::Index(b)]),
 				}
 			},
 		}
@@ -74,115 +84,139 @@ mod test {
 	#[test]
 	fn new_test() {
 		assert_eq!(
-			Grid::new(0),
-			Grid {
-				top: 0,
-				middle: None,
-				bottom: 0,
-			}
-		);
-		assert_eq!(
 			Grid::new(1),
 			Grid {
-				top: 1,
+				top: vec![Buffer::Index(0)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			}
 		);
 		assert_eq!(
 			Grid::new(2),
 			Grid {
-				top: 2,
+				top: vec![Buffer::Index(0), Buffer::Index(1)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			}
 		);
 		assert_eq!(
 			Grid::new(3),
 			Grid {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 0,
+				bottom: None,
 			}
 		);
 		assert_eq!(
 			Grid::new(4),
 			Grid {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 1,
+				bottom: Some(vec![Buffer::Index(3)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(5),
 			Grid {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 2,
+				bottom: Some(vec![Buffer::Index(3), Buffer::Index(4)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(6),
 			Grid {
-				top: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
 				middle: None,
-				bottom: 3,
+				bottom: Some(vec![Buffer::Index(3), Buffer::Index(4), Buffer::Index(5)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(7),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Half]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![MiddleBuffer::One(Buffer::Index(3))]),
+				bottom: Some(vec![Buffer::Index(4), Buffer::Index(5), Buffer::Index(6)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(8),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4))]),
+				bottom: Some(vec![Buffer::Index(5), Buffer::Index(6), Buffer::Index(7)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(9),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full, Row::Half]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::One(Buffer::Index(5)),
+				]),
+				bottom: Some(vec![Buffer::Index(6), Buffer::Index(7), Buffer::Index(8)]),
+			}
+		);
+		assert_eq!(
+			Grid::new(9),
+			Grid {
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::Two(Buffer::Index(5), Buffer::Index(6)),
+				]),
+				bottom: Some(vec![Buffer::Index(7), Buffer::Index(8), Buffer::Index(9)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(10),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full, Row::Full]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::Two(Buffer::Index(5), Buffer::Index(6)),
+					MiddleBuffer::One(Buffer::Index(7)),
+				]),
+				bottom: Some(vec![Buffer::Index(8), Buffer::Index(9), Buffer::Index(10)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(11),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full, Row::Full, Row::Half]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::Two(Buffer::Index(5), Buffer::Index(6)),
+					MiddleBuffer::Two(Buffer::Index(7), Buffer::Index(8)),
+				]),
+				bottom: Some(vec![Buffer::Index(9), Buffer::Index(10), Buffer::Index(11)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(12),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full, Row::Full, Row::Full]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::Two(Buffer::Index(5), Buffer::Index(6)),
+					MiddleBuffer::Two(Buffer::Index(7), Buffer::Index(8)),
+					MiddleBuffer::One(Buffer::Index(9)),
+				]),
+				bottom: Some(vec![Buffer::Index(10), Buffer::Index(11), Buffer::Index(12)]),
 			}
 		);
 		assert_eq!(
 			Grid::new(13),
 			Grid {
-				top: 3,
-				middle: Some(vec![Row::Full, Row::Full, Row::Full, Row::Half]),
-				bottom: 3,
+				top: vec![Buffer::Index(0), Buffer::Index(1), Buffer::Index(2)],
+				middle: Some(vec![
+					MiddleBuffer::Two(Buffer::Index(3), Buffer::Index(4)),
+					MiddleBuffer::Two(Buffer::Index(5), Buffer::Index(6)),
+					MiddleBuffer::Two(Buffer::Index(7), Buffer::Index(8)),
+					MiddleBuffer::Two(Buffer::Index(9), Buffer::Index(10)),
+				]),
+				bottom: Some(vec![Buffer::Index(11), Buffer::Index(12), Buffer::Index(13)]),
 			}
 		);
 	}
